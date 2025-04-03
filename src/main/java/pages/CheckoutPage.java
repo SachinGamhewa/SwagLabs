1b58058370b8f91125b3700c8cf0fa3cdcc581ad
Sachin Gamhewa <107618727+SachinGamhewa@users.noreply.github.com>
2025-04-03 21:52:05 +0530

    Update CheckoutPage.java

--- src/main/java/pages/CheckoutPage.java
+++ src/main/java/pages/CheckoutPage.java
@@ -1,1 +1,33 @@
+package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutPage {
+
+    private WebDriver driver;
+    public CheckoutPage (WebDriver driver) {
+        this.driver=driver;
+    }
+
+    private By FirstNameInputBox= new By.ById("first-name");
+    public void fillFirstNameInputBox(String text){
+        driver.findElement(FirstNameInputBox).sendKeys(text);
+    }
+
+    private By LastNameInputBox= new By.ById("last-name");
+    public void fillLastNameInputBox(String text){
+        driver.findElement(LastNameInputBox).sendKeys(text);
+    }
+
+    private By PostalCodeInputBox= new By.ById("postal-code");
+    public void fillPostalCodeInputBox(String text){
+        driver.findElement(PostalCodeInputBox).sendKeys(text);
+    }
+
+    private By ContinueToSecondCheckoutButton= new By.ById("continue");
+    public SecondCheckoutPage clickContinueToSecondCheckoutButton(){
+        driver.findElement(ContinueToSecondCheckoutButton).click();
+        return new SecondCheckoutPage(driver);
+    }
+}

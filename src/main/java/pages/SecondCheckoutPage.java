8d0f31bc53dd63d646becbce33d6e5032a806945
Sachin Gamhewa <107618727+SachinGamhewa@users.noreply.github.com>
2025-04-03 22:15:51 +0530

    Update SecondCheckoutPage.java

--- src/main/java/pages/SecondCheckoutPage.java
+++ src/main/java/pages/SecondCheckoutPage.java
@@ -1,1 +1,28 @@
+package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SecondCheckoutPage {
+
+    private WebDriver driver;
+    public SecondCheckoutPage (WebDriver driver) {
+        this.driver=driver;
+    }
+
+    public String getCurrentURL(){
+        return driver.getCurrentUrl();
+    }
+
+    private By FinishButton= new By.ById("finish");
+
+    public void clickOnFinishButton(){
+        driver.findElement(FinishButton).click();
+    }
+
+
+    private final String expectedURLAfterSuccessfulOrder="https://www.saucedemo.com/checkout-complete.html";
+    public String getExpectedURLAfterSuccessfulOrder(){
+        return expectedURLAfterSuccessfulOrder;
+    }
+}

dca731ec1621cf37b021caa80522fdf68dca97fb
Sachin Gamhewa <107618727+SachinGamhewa@users.noreply.github.com>
2025-04-03 21:35:04 +0530

    Update CartPage.java

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -1,1 +1,18 @@
+package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+
+    private WebDriver driver;
+    public CartPage (WebDriver driver) {
+        this.driver=driver;
+    }
+
+    private By CheckoutButton= new By.ById("checkout");
+    public CheckoutPage clickCheckoutButton(){
+        driver.findElement(CheckoutButton).click();
+        return new CheckoutPage(driver);
+    }
+}

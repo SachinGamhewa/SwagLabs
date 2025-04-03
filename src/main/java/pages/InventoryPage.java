502eae140f1e0723ffe8bb5cabf9a2ac0f12f000
Sachin Gamhewa <107618727+SachinGamhewa@users.noreply.github.com>
2025-04-03 21:26:16 +0530

    Update InventoryPage.java

--- src/main/java/pages/InventoryPage.java
+++ src/main/java/pages/InventoryPage.java
@@ -1,1 +1,47 @@
+package pages;
 
+import org.openqa.selenium.*;
+
+public class InventoryPage {
+    private final String expectedURLBeforeLogin="https://www.saucedemo.com/";
+    private final String expectedURLAfterLogin="https://www.saucedemo.com/inventory.html";
+
+    private By ShoppingCartLink = new By.ByClassName("shopping_cart_link");
+    private By BackpackAddToCartButton = By.id("add-to-cart-sauce-labs-backpack");
+
+    private WebDriver driver;
+    public InventoryPage(WebDriver driver) {
+        this.driver=driver;
+    }
+
+    public String getExpectedURLAfterLogin(){
+        return expectedURLAfterLogin;
+    }
+
+    public String getExpectedURLBeforeLogin(){
+        return expectedURLBeforeLogin;
+    }
+
+    public String getCurrentURL(){
+        return driver.getCurrentUrl();
+    }
+
+    public void clickAddToCartButton(By element)
+    {
+        driver.findElement(element).click();
+    }
+
+    public void clickBackpackAddToCartButton(){
+        clickAddToCartButton(BackpackAddToCartButton);
+    }
+
+    public CartPage clickShoppingCartLink(){
+        driver.findElement(ShoppingCartLink).click();
+        return new CartPage(driver);
+    }
+
+    public void getInventoryPage(){
+        driver.get(expectedURLAfterLogin);
+    }
+
+}

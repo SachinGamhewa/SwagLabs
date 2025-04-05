ef5d6f0c923e900646074589273533e982a9fd06
Sachin Gamhewa <107618727+SachinGamhewa@users.noreply.github.com>
2025-04-05 13:58:57 +0530

    Update BaseTests.java

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,1 +1,40 @@
+package base;
 
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pages.LoginPage;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BaseTests {
+
+    private  WebDriver driver;
+    protected  LoginPage loginPage;
+
+    @BeforeAll
+    public  void setUp() {      
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--guest");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--avoid-stats");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @BeforeEach
+    public void getToLoginPage(){
+        loginPage = new LoginPage(driver);
+        loginPage.getLoginPage();
+    }
+
+    @AfterAll
+    public  void tearDown()
+    {
+        driver.quit();
+    }
+  
+}
